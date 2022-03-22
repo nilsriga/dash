63ecff2fa6b37861431d030950c3394604e211d7
snotrman <siafrontera1@gmail.com>
2022-03-22 07:47:09 +0200

    init

---
+++ precache-manifest.ff70848c035ffadc933dfe60cf0e8755.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3bf7d0105c8448a7cde2",
+    "url": "/cryptoboys-nft-marketplace/static/css/main.6417e573.chunk.css"
+  },
+  {
+    "revision": "3bf7d0105c8448a7cde2",
+    "url": "/cryptoboys-nft-marketplace/static/js/main.52211c68.chunk.js"
+  },
+  {
+    "revision": "1a1dbebc82ed738db43a",
+    "url": "/cryptoboys-nft-marketplace/static/js/runtime~main.f255ff12.js"
+  },
+  {
+    "revision": "c955e1d44cc6f0270eca",
+    "url": "/cryptoboys-nft-marketplace/static/js/2.b0da93a0.chunk.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/cryptoboys-nft-marketplace/static/media/loading.26ef8bb4.gif"
+  },
+  {
+    "revision": "fbf33967fa244d21d61fb85f233fc331",
+    "url": "/cryptoboys-nft-marketplace/static/media/metamask.fbf33967.svg"
+  },
+  {
+    "revision": "a2b83230a1dd8f5321cf2193ca972c70",
+    "url": "/cryptoboys-nft-marketplace/index.html"
+  }
+];
